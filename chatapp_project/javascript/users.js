58db349474c41e4ce57a592dbbb49ce750586de4
manan2425 <131514862+manan2425@users.noreply.github.com>
2024-06-07 12:24:09 +0530

    Add files via upload

---
+++ chatapp_project/javascript/users.js
@@ -0,0 +1,67 @@
+const searchBar = document.querySelector(".search input"),
+searchIcon = document.querySelector(".search button"),
+usersList = document.querySelector(".users-list");
+
+searchIcon.onclick = ()=>{
+  searchBar.classList.toggle("show");
+  searchIcon.classList.toggle("active");
+  searchBar.focus();
+  if(searchBar.classList.contains("active")){
+    searchBar.value = "";
+    searchBar.classList.remove("active");
+  }
+}
+
+searchBar.onkeyup = ()=>{
+  let searchTerm = searchBar.value;
+  if(searchTerm != ""){
+    searchBar.classList.add("active");
+  }else{
+    searchBar.classList.remove("active");
+  }
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "php/search.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState === XMLHttpRequest.DONE){
+        if(xhr.status === 200){
+          let data = xhr.response;
+          usersList.innerHTML = data;
+        }
+    }
+  }
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send("searchTerm=" + searchTerm);
+}
+
+setInterval(() =>{
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "php/users.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState === XMLHttpRequest.DONE){
+        if(xhr.status === 200){
+          let data = xhr.response;
+          if(!searchBar.classList.contains("active")){
+            usersList.innerHTML = data;
+          }
+        }
+    }
+  }
+  xhr.send();
+}, 500);
+
+var isviewing = false;
+function info(){
+
+  var profilephoto = document.getElementById("profile");
+  var profilename = document.getElementById("profile_name");
+  if(isviewing == false){
+    profilephoto.classList.add("viewprofilephoto");
+    profilename.classList.add("viewpphoto");
+    isviewing = true;
+  }
+  else{
+    profilephoto.classList.remove("viewprofilephoto");
+    profilename.classList.remove("viewpphoto");
+    isviewing = false;
+  }
+}
